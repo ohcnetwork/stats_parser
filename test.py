3365a35030855261b41f03630c8875238bec3e45
saanuregh <saanu09xs@gmail.com>
2020-03-27 15:20:07 +0530

    fixed test

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ class Object(object):
 if __name__ == "__main__":
     url = sys.argv[1]
     request = Object()
-    request.args = {"url": url}
+    request.args = {"url": url, "test": "1"}
     print(f"Parsing url:{url}\n")
     res = parse_pdf(request)
     print(res)
