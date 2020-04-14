09a53279b26e55e0dde5cd6c47e441eb074c1a4b
saanuregh <saanu09xs@gmail.com>
2020-04-15 03:01:46 +0530

    retired

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+## Unmaintained
 # Running it locally
 * Requires [Ghostscript](https://www.ghostscript.com/)
   * for debian based distros (eg. ubuntu) run `sudo apt install ghostscript`
 * Requires Python 3.6 or above
 * Install python dependency by running `pip install -r requirement.txt`
-* Run `python test.py <url to pdf>`
+* Run `python main.py <url to pdf>`
--- main.py
+++ main.py
@@ -4,75 +4,53 @@ import json
 import requests
 from datetime import datetime
 import os
+import sys
 
-
-def parse_pdf(request, db=False):
-    test = 0
-    try:
-        content_type = request.headers["content-type"]
-    except:
-        pass
-    if request.args and "url" in request.args:
-        url = request.args.get("url")
-        if "test" in request.args:
-            test = int(request.args.get("test"))
-    elif content_type == "application/json":
-        request_json = request.get_json(silent=True)
-        if request_json and "text" in request_json:
-            url = request_json["text"]
-        else:
-            msg = "JSON is invalid, or missing a 'text' property"
-            if not test:
-                send_err_res(msg)
-            raise ValueError(msg)
-    else:
-        msg = "No 'url' in param or wrong content type"
-        if not test:
-            send_err_res(msg)
-        raise ValueError(msg)
-    try:
-        data = parse(url)
-        if not test:
-            send_res(request, data, db)
-        if db:
-            return data
-        return json.dumps(data)
-    except Exception as e:
-        if not test:
-            send_err_res(getattr(e, "message", repr(e)))
-        raise e
-
-
-def parse_pdf_update(request):
-    json_data = parse_pdf(request, db=True)
-    # TODO do stuff here to update db
-    return "Updating db..."
+disricts = [
+    {"name": "Thiruvananthapuram", "alt": ["Thiriruvanathapuram"]},
+    {"name": "Kollam", "alt": []},
+    {"name": "Pathanamthitta", "alt": []},
+    {"name": "Idukki", "alt": []},
+    {"name": "Kottayam", "alt": []},
+    {"name": "Alappuzha", "alt": []},
+    {"name": "Ernakulam", "alt": []},
+    {"name": "Thrissur", "alt": []},
+    {"name": "Palakkad", "alt": []},
+    {"name": "Malappuram", "alt": []},
+    {"name": "Kozhikode", "alt": []},
+    {"name": "Wayanad", "alt": []},
+    {"name": "Kannur", "alt": ["Kannu"]},
+    {"name": "Kasaragod", "alt": ["Kasargod"]},
+]
 
 
 def parse(url):
-    tables = camelot.read_pdf(url, pages="all", flavor="lattice")
-    surv, chro, dist = 0, [], 0
+    tables = camelot.read_pdf(url, pages="all")
+    surv, chro, dist = 0, [], []
     for table in tables:
-        if table.df[len(table.df.columns) - 1][0] == "Remarks":
-            chro.append(table.df)
-        if table.df[0][0] == "District":
-            if len(table.df.columns) == 5:
-                surv = table.df
+        if len(table.df.columns) in [2, 3]:
+            dist.append(table.df)
+        if len(table.df.columns) in [4, 5]:
+            if table.df[len(table.df.columns) - 1][0] == "Remarks":
+                chro.append(table.df)
             if table.df[0][0] == "District":
-                dist = table.df
-    # manual fix for bule_25032020.pdf
+                surv = table.df
+    data = init_data()
+    num, rem, dis = "", "", ""
+
+    # manual fixes
+    # for bule_25032020.pdf
     if "bule_25032020" in url:
-        print(url, "had to make some manual changes")
         chro[0][2][4] = "Pathanamthitta – 4\nKottayam – 2 \nErnakulam -2"
-    # manual fix for bule_20032020.pdf
+    # for bule_20032020.pdf
     if "bule_20032020" in url:
         chro[0][1][9] = "Thiruvananthapuram -3"
         chro[0][1][10] = "Thiruvananthapuram -1"
-    data = init_data()
+
+    # Parses table: Chronology of Positive cases
     i = 1
     if "patient" in chro[0][0][0]:
         i = 0
-    num, rem, dis = "", "", ""
 
     def dis_parse(s):
         return list(
@@ -86,6 +64,13 @@ def parse(url):
                 ).splitlines(),
             )
         )
+
+    def add(n):
+        data[t[0]]["corona_positive"] += n
+        if "Negative" in rem:
+            data[t[0]]["cured_discharged"] += n
+        if "Expired" in status:
+            data[t[0]]["deaths"] += n
 
     for ch in chro:
         for row in ch.iterrows():
@@ -105,111 +90,56 @@ def parse(url):
             if len(dis) > 1:
                 if len(dis[0]) > 1:
                     for t in dis:
-                        # manual fix for misspelled Thiruvananthapuram
-                        if t[0]:
-                            if t[0] == "Thiriruvanathapuram":
-                                t[0] = "Thiruvananthapuram"
-                            data[t[0]]["corona_positive"] = data[t[0]][
-                                "corona_positive"
-                            ] + int(t[1])
-                            if "Negative" in rem:
-                                data[t[0]]["cured_discharged"] = data[t[0]][
-                                    "cured_discharged"
-                                ] + int(t[1])
-                            if "Expired" in status:
-                                data[t[0]]["deaths"] = data[t[0]]["deaths"] + int(t[1])
+                        t[0] = check_alt(t[0])
+                        add(int(t[1]))
                 else:
                     for t in dis:
-                        # manual fix for 'Kasaragod' misspelled as 'Kasargod'
-                        if t[0] == "Kasargod":
-                            t[0] = "Kasaragod"
-                        data[t[0]]["corona_positive"] = (
-                            data[t[0]]["corona_positive"] + 1
-                        )
-                        if "Negative" in rem:
-                            data[t[0]]["cured_discharged"] = (
-                                data[t[0]]["cured_discharged"] + 1
-                            )
-                        if "Expired" in status:
-                            data[t[0]]["deaths"] = data[t[0]]["deaths"] + 1
+                        t[0] = check_alt(t[0])
+                        add(1)
             else:
                 if len(dis[0]) > 1:
                     for t in dis:
-                        if t[0] == "Thiriruvanathapuram":
-                            t[0] = "Thiruvananthapuram"
-                        if t[0] == "Kannu":
-                            t[0] = "Kannur"
-                        if t[0]:
-                            data[t[0]]["corona_positive"] = data[t[0]][
-                                "corona_positive"
-                            ] + int(t[1])
-                            if "Negative" in rem:
-                                data[t[0]]["cured_discharged"] = data[t[0]][
-                                    "cured_discharged"
-                                ] + int(t[1])
-                            if "Expired" in status:
-                                data[t[0]]["deaths"] = data[t[0]]["deaths"] + int(t[1])
+                        t[0] = check_alt(t[0])
+                        add(int(t[1]))
+
                 else:
+                    t = dis[0]
+                    t[0] = check_alt(t[0])
                     inc = 1
-                    if (row[1][i]):
+                    if row[1][i]:
                         inc = int(num)
-                    for t in dis[0]:
-                        data[t]["corona_positive"] = data[t]["corona_positive"] + inc
-                        if "Negative" in rem:
-                            data[t]["cured_discharged"] = data[t][
-                                "cured_discharged"
-                            ] + inc
-                        if "Expired" in status:
-                            data[t]["deaths"] = data[t][
-                                "deaths"
-                            ] + inc
+                    add(inc)
+
+    # Parses table: Details of persons under Surveillance
     for row in surv.iterrows():
-        if "District" in row[1][0]:
+        if any(x in row[1][0] for x in ["District", "Total"]):
             continue
-        if "Total" in row[1][0]:
-            continue
-        t = row[1][0].strip()
-        data[t]["under_observation"] = data[t]["under_observation"] + int(row[1][1])
-        data[t]["under_home_isolation"] = data[t]["under_home_isolation"] + int(
-            row[1][2]
-        )
-        data[t]["total_hospitalised"] = data[t]["total_hospitalised"] + int(row[1][3])
-        data[t]["hospitalised_today"] = data[t]["hospitalised_today"] + int(row[1][4])
-    for row in dist.iterrows():
-        if "District" in row[1][0]:
-            continue
-        if "Total" in row[1][0]:
-            continue
-        t = row[1][0].strip()
-        data[t]["positive_admitted"] = data[t]["positive_admitted"] + int(row[1][1])
-    _data = {
+        t = check_alt(row[1][0].strip())
+        data[t]["under_observation"] += int(row[1][1])
+        data[t]["under_home_isolation"] += int(row[1][2])
+        data[t]["total_hospitalised"] += int(row[1][3])
+        data[t]["hospitalised_today"] += int(row[1][4])
+
+    # Parses table: District wise distribution based on hospital admission
+    for di in dist:
+        for row in di.iterrows():
+            if any(x in row[1][0] for x in ["District", "Total"]):
+                continue
+            t = check_alt(row[1][0].strip())
+            data[t]["positive_admitted"] += int(row[1][1])
+
+    data = {
         "kerala": data,
         "time": datetime.now().isoformat(),
         "file_url": url,
     }
-    return _data
+    return json.dumps(data)
 
 
 def init_data():
     data = {}
-    disricts = [
-        "Thiruvananthapuram",
-        "Kollam",
-        "Pathanamthitta",
-        "Idukki",
-        "Kottayam",
-        "Alappuzha",
-        "Ernakulam",
-        "Thrissur",
-        "Palakkad",
-        "Malappuram",
-        "Kozhikode",
-        "Wayanad",
-        "Kannur",
-        "Kasaragod",
-    ]
     for d in disricts:
-        data[d] = {
+        data[d["name"]] = {
             "under_observation": 0,
             "under_home_isolation": 0,
             "total_hospitalised": 0,
@@ -222,54 +152,13 @@ def init_data():
     return data
 
 
-def send_res(request, data, db=False):
-    webhook_url = os.environ.get("WEBHOOK")
-    print(webhook_url)
-    blocks = [
-        {
-            "type": "section",
-            "text": {"type": "mrkdwn", "text": f"Parsed <{data['file_url']}>"},
-        }
-    ]
-    for d in data["kerala"]:
-        blocks.append(
-            {"type": "section", "text": {"type": "mrkdwn", "text": f"*{d}:*"},}
-        )
-        tmp = []
-        for key, value in data["kerala"][d].items():
-            tmp.append({"type": "mrkdwn", "text": f"{key}:{value}"})
-        blocks.append({"type": "section", "fields": tmp})
-    if db:
-        blocks.append(
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": "*Updating Database...*"},
-            }
-        )
-    payload = {
-        "blocks": json.dumps(blocks),
-        "username": "parser_bot",
-        "icon_emoji": ":robot_face:",
-    }
-    response = requests.post(
-        webhook_url,
-        data=json.dumps(payload),
-        headers={"Content-Type": "application/json"},
-    )
-    print(response)
+def check_alt(dist):
+    for d in disricts:
+        if dist in d["alt"]:
+            return d["name"]
+    return dist
 
 
-def send_err_res(msg):
-    webhook_url = os.environ.get("WEBHOOK")
-    print(webhook_url)
-    payload = {
-        "text": msg,
-        "username": "parser_bot",
-        "icon_emoji": ":robot_face:",
-    }
-    response = requests.post(
-        webhook_url,
-        data=json.dumps(payload),
-        headers={"Content-Type": "application/json"},
-    )
-    print(response)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    print(parse(url))
